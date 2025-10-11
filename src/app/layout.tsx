f54a54026303d4a7cb04fcc92511e5d595efdbc3
farismnrr <farismunir2@gmail.com>
2025-10-12 00:43:52 +0700

    feat: update login page text encoding, refactor imports, and add global CSS declaration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
+import "@/app/globals";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 import { ThemeProvider } from "@/components/providers/themeProvider";
 
 const geistSans = Geist({
